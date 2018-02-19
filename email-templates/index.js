fdc18b005ef379f2e20cc33615762b279fa57109
Jenky Nolasco <jenky.nolasco@gmail.com>
2018-02-19 12:02:15 -0500

    Small fix/Added mail templates

--- email-templates/index.js
+++ email-templates/index.js
@@ -3298,7 +3298,6 @@ module.exports = {
                               <div style="border-top: 0px solid transparent; border-left: 0px solid transparent; border-bottom: 0px solid transparent; border-right: 0px solid transparent; padding-top:5px; padding-bottom:5px; padding-right: 0px; padding-left: 0px;">
                                 <!--<![endif]-->
 
-
                                 <div class="">
                                   <!--[if mso]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding-right: 10px; padding-left: 10px; padding-top: 10px; padding-bottom: 0px;"><![endif]-->
                                   <div style="color:#000;line-height:120%;font-family:'Roboto', Tahoma, Verdana, Segoe, sans-serif; padding-right: 10px; padding-left: 10px; padding-top: 10px; padding-bottom: 0px;">
@@ -3326,46 +3325,6 @@ module.exports = {
                         </div>
                       </div>
                     </div>
-                <!---- -->
-                      <div style="Margin: 0 auto;min-width: 320px;max-width: 780px;overflow-wrap: break-word;word-wrap: break-word;word-break: break-word;background-color: transparent;"
-                        class="block-grid ">
-                        <div style="border-collapse: collapse;display: table;width: 100%;background-color:transparent;">
-                          <!--[if (mso)|(IE)]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="background-color:transparent;" align="center"><table cellpadding="0" cellspacing="0" border="0" style="width: 780px;"><tr class="layout-full-width" style="background-color:transparent;"><![endif]-->
-
-                          <!--[if (mso)|(IE)]><td align="center" width="780" style=" width:780px; padding-right: 0px; padding-left: 0px; padding-top:5px; padding-bottom:5px; border-top: 0px solid transparent; border-left: 0px solid transparent; border-bottom: 0px solid transparent; border-right: 0px solid transparent;" valign="top"><![endif]-->
-                          <div class="col num12" style="min-width: 320px;max-width: 780px;display: table-cell;vertical-align: top;">
-                            <div style="background-color: transparent; width: 100% !important;">
-                              <!--[if (!mso)&(!IE)]><!-->
-                              <div style="border-top: 0px solid transparent; border-left: 0px solid transparent; border-bottom: 0px solid transparent; border-right: 0px solid transparent; padding-top:5px; padding-bottom:5px; padding-right: 0px; padding-left: 0px;">
-                                <!--<![endif]-->
-
-
-                                <div class="">
-                                  <!--[if mso]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding-right: 10px; padding-left: 10px; padding-top: 10px; padding-bottom: 0px;"><![endif]-->
-                                  <div style="color:#000;line-height:120%;font-family:'Roboto', Tahoma, Verdana, Segoe, sans-serif; padding-right: 10px; padding-left: 10px; padding-top: 10px; padding-bottom: 0px;">
-                                    <div style="font-size:12px;line-height:14px;color:#000;font-family:'Roboto', Tahoma, Verdana, Segoe, sans-serif;text-align:left;">
-                                      <p style="margin: 0;font-size: 14px;line-height: 17px">
-                                        <span style="font-size: 18px; line-height: 21px;">
-                                          <strong>
-                                            <span style="color: #000; line-height: 21px; font-size: 18px;">Applicant Information</span>
-                                          </strong>
-                                        </span>
-                                      </p>
-                                    </div>
-                                  </div>
-                                  <!--[if mso]></td></tr></table><![endif]-->
-                                </div>
-
-                                <!--[if (!mso)&(!IE)]><!-->
-                              </div>
-                              <!--<![endif]-->
-                            </div>
-                          </div>
-                          <!--[if (mso)|(IE)]></td></tr></table></td></tr></table><![endif]-->
-                        </div>
-                      </div>
-                    </div>
-                <!---- -->
                     <div style="background-color:transparent;">
                       <div style="Margin: 0 auto;min-width: 320px;max-width: 780px;overflow-wrap: break-word;word-wrap: break-word;word-break: break-word;background-color: transparent;"
                         class="block-grid ">
